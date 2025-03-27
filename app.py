ca7aa142795fb8e3abdd0559710f591cc68826fd
Luis Esteban Serrano <leserranob@outlook.com>
2025-03-27 11:00:56 -0600

    Prueba Interfaz grafica QT

--- app.py
+++ app.py
@@ -7,9 +7,24 @@ from PySide6.QtCore import *
 from GUI.pesaje_UI import Ui_MainWindow # Código generado por QtDesigner
 
 class MainWindow(QMainWindow, Ui_MainWindow):
+    num = 18
+
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        self.lcdNumber.display(self.num)
+
+        self.btnTarar.clicked.connect(self.tarar)
+        self.btnPesar.clicked.connect(self.pesar)
+
+    def tarar(self):
+        self.num = 0
+        self.lcdNumber.display(self.num)
+
+    def pesar(self):
+        self.num += 1
+        self.lcdNumber.display(self.num)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
