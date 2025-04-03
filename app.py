652ae1a187fea57abda2919d754e16b73a774848
Luis Esteban Serrano <leserranob@outlook.com>
2025-04-03 12:55:35 -0600

    Leyendo registro de status

--- app.py
+++ app.py
@@ -26,31 +26,43 @@ print("Resp: ", resp)
 
 while True:
     print("Status: ")
-    msg = [0xF3, 0x00]
+    msg = [0xF3]
     resp = spi.xfer2(msg)
     print("resp: " + resp + "[0]: " + resp[0] + " [1]: " + resp[1])
+    print(1 & resp[0])
+
+    if (8 & resp[0]) == 0:
+        print("Transfiriendo...")
+    else:
+        print("Midiendo...")
+
+    if (1 & resp[0]) == 0:
+        print("Copiado al registro, se puede leer")
+    else:
+        print("Copiando...")
+
     # print("0 updated", 1 & resp[0])
 
-    print("\n================================\n")
+    # print("\n================================\n")
 
-    print("Humedad: ")
-    msg = [0xFD, 0xFE]
-    resp = spi.xfer2(msg)
-    print(resp)
+    # print("Humedad: ")
+    # msg = [0xFD, 0xFE]
+    # resp = spi.xfer2("FD: ", msg[0], "FE: ", msg[1])
+    # print(resp)
 
-    print("\n================================\n")
+    # print("\n================================\n")
 
-    print("Temperatura: ")
-    msg = [0xFA, 0xFB, 0xFC]
-    resp = spi.xfer2(msg)
-    print(resp)
+    # print("Temperatura: ")
+    # msg = [0xFA, 0xFB, 0xFC]
+    # resp = spi.xfer2(msg)
+    # print(resp)
 
-    print("\n================================\n")
+    # print("\n================================\n")
 
-    print("Presion: ")
-    msg = [0xF7, 0xF8, 0xF9]
-    resp = spi.xfer2(msg)
-    print(resp)
+    # print("Presion: ")
+    # msg = [0xF7, 0xF8, 0xF9]
+    # resp = spi.xfer2(msg)
+    # print(resp)
 
-    print("\n================================\n")
+    # print("\n================================\n")
     time.sleep(1)
